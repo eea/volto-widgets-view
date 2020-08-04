02e8442e84a83727dbfe3f6b5f872a00aa6d4fa9
Alin Voinea <contact@avoinea.com>
2020-08-04 20:19:56 +0300

    Add TextWidget tests

---
+++ src/components/theme/Widgets/TextWidget.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { TextWidget } from "./TextWidget";
+
+describe("TextWidget", () => {
+  it("renders a TextWidget", () => {
+    const component = renderer.create(
+      <TextWidget
+        data="Foo bar"
+        schema={{id: "description"}}
+        className="metadata"
+      />
+    );
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+});
