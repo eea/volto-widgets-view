8ce92bce25c29d7485b644bc543cf54b8ebb25cb
Alex Medesan <alex.medesan@gmail.com>
2021-10-18 11:43:24 +0300

    consider date config

--- src/components/theme/Widgets/DateWidget.jsx
+++ src/components/theme/Widgets/DateWidget.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import cx from 'classnames';
 import moment from 'moment';
+import config from '@plone/volto/registry';
 
 export const DateWidget = ({ value, children, className }) =>
   value ? (
     <span className={cx(className, 'date', 'widget')}>
       {children
-        ? children(moment(value).format('ll'))
-        : moment(value).format('ll')}
+        ? children(moment(value).format(config.settings.dateFormat || 'll'))
+        : moment(value).format(config.settings.dateFormat || 'll')}
     </span>
   ) : (
     ''
--- src/components/theme/Widgets/DatetimeWidget.jsx
+++ src/components/theme/Widgets/DatetimeWidget.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import cx from 'classnames';
 import moment from 'moment';
+import config from '@plone/volto/registry';
 
 export const DatetimeWidget = ({ value, children, className }) =>
   value ? (
     <span className={cx(className, 'datetime', 'widget')}>
       {children
-        ? children(moment(value).format('lll'))
-        : moment(value).format('lll')}
+        ? children(moment(value).format(config.settings.dateFormat || 'lll'))
+        : moment(value).format(config.settings.dateFormat || 'lll')}
     </span>
   ) : (
     ''
