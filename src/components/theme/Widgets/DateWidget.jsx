f425e1980697f519cd8b0a26ae6945fbcbfc821d
Alin Voinea <contact@avoinea.com>
2021-10-19 20:39:20 +0300

    Add date locale config settings

--- src/components/theme/Widgets/DateWidget.jsx
+++ src/components/theme/Widgets/DateWidget.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import cx from 'classnames';
 import moment from 'moment';
+import config from '@plone/volto/registry';
 
-export const DateWidget = ({ value, children, className }) =>
-  value ? (
+export const DateWidget = ({ value, children, className }) => {
+  moment.locale(config.settings.dateLocale);
+  return value ? (
     <span className={cx(className, 'date', 'widget')}>
       {children
         ? children(moment(value).format('ll'))
@@ -12,3 +14,4 @@ export const DateWidget = ({ value, children, className }) =>
   ) : (
     ''
   );
+};
--- src/components/theme/Widgets/DatetimeWidget.jsx
+++ src/components/theme/Widgets/DatetimeWidget.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import cx from 'classnames';
 import moment from 'moment';
+import config from '@plone/volto/registry';
 
-export const DatetimeWidget = ({ value, children, className }) =>
-  value ? (
+export const DatetimeWidget = ({ value, children, className }) => {
+  moment.locale(config.settings.dateLocale);
+  return value ? (
     <span className={cx(className, 'datetime', 'widget')}>
       {children
         ? children(moment(value).format('lll'))
@@ -12,3 +14,4 @@ export const DatetimeWidget = ({ value, children, className }) =>
   ) : (
     ''
   );
+};
